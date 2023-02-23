21e4bd9caca6817a553663ab9096a8e683cb6c94
milosdjurica <milosdjurica13@gmail.com>
2023-02-23 18:30:08 +0100

    create customer, edit customer, delete customer

--- pages/api/customers/[customerId].tsx
+++ pages/api/customers/[customerId].tsx
@@ -3,26 +3,72 @@ import { Customer } from "@/pages/customers";
 import { ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export const getCustomer = async (customerId: string): Promise<Customer> => {
+type UpdateCustomerType = {
+    name?: string;
+    industry?: string;
+};
+
+export const getCustomer = async (customerId: ObjectId): Promise<Customer> => {
     const mongoClient = await clientPromise;
     const customer = (await mongoClient
         .db()
         .collection("customers")
-        .findOne({ _id: new ObjectId(customerId as string) })) as Customer;
+        .findOne({ _id: customerId })) as Customer;
 
     return JSON.parse(JSON.stringify(customer));
 };
 
+export const editCustomer = async (
+    customerId: ObjectId,
+    updatedCustomer: UpdateCustomerType
+) => {
+    const mongoClient = await clientPromise;
+    return await mongoClient
+        .db()
+        .collection("customers")
+        .replaceOne({ _id: customerId }, updatedCustomer);
+};
+
+export const deleteCustomer = async (customerId: ObjectId) => {
+    const mongoClient = await clientPromise;
+    return await mongoClient
+        .db()
+        .collection("customers")
+        .deleteOne({ _id: customerId });
+};
+
 export default async (
     req: NextApiRequest,
-    res: NextApiResponse<Customer | string>
+    res: NextApiResponse<
+        | Customer
+        | { modifiedCount: number }
+        | { deletedCount: number }
+        | { error: string }
+    >
 ) => {
     try {
-        const { customerId } = req.query;
-        const customer = await getCustomer(customerId as string);
-        if (!customer) res.status(404).json("Customer not found!");
-        res.status(200).json(customer);
+        const customerId = new ObjectId(req.query.customerId! as string);
+        if (req.method === "GET") {
+            const customer = await getCustomer(customerId);
+            if (!customer)
+                return res.status(404).json({ error: "Customer not found!" });
+            res.status(200).json(customer);
+        } else if (req.method === "PATCH") {
+            const updateResponse = await editCustomer(customerId, {
+                name: req.body.name,
+                industry: req.body.industry,
+            });
+            console.log("izasao");
+            res.status(200).json({
+                modifiedCount: updateResponse.modifiedCount,
+            });
+        } else if (req.method === "DELETE") {
+            const data = await deleteCustomer(customerId);
+            res.status(200).json({ deletedCount: data.deletedCount });
+        }
     } catch (error) {
-        res.status(404).json("Customer not found! Check if ID is valid");
+        res.status(404).json({
+            error: "Server error, customer not found! Check if ID is valid",
+        });
     }
 };
--- pages/api/customers/index.tsx
+++ pages/api/customers/index.tsx
@@ -1,6 +1,12 @@
 import clientPromise from "@/lib/mongodb";
 import { Customer } from "@/pages/customers";
+import { ObjectId } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
+
+type CreateCustomerType = {
+    name: string;
+    industry: string;
+};
 
 export const getAllCustomers = async (): Promise<Customer[]> => {
     try {
@@ -17,10 +23,44 @@ export const getAllCustomers = async (): Promise<Customer[]> => {
     }
 };
 
-export default async (
-    req: NextApiRequest,
-    res: NextApiResponse<Customer[]>
-) => {
-    const customers = await getAllCustomers();
-    res.status(200).json(customers);
+export const createCustomer = async (
+    customer: CreateCustomerType
+): Promise<ObjectId | string> => {
+    try {
+        const mongoClient = await clientPromise;
+
+        const newCustomer = await mongoClient
+            .db()
+            .collection("customers")
+            .insertOne(customer);
+
+        return newCustomer.insertedId;
+    } catch (error) {
+        return "Something went wrong!";
+    }
 };
+
+export default async function handler(req: any, res: any) {
+    try {
+        if (req.method === "GET") {
+            const customers = await getAllCustomers();
+            res.status(200).json(customers);
+        } else if (req.method === "POST") {
+            if (!req.body.name || !req.body.industry) {
+                return res.status(400).json("Name and industry are required!");
+            }
+
+            const customer: CreateCustomerType = {
+                name: req.body.name,
+                industry: req.body.industry,
+            };
+
+            const createdId = await createCustomer(customer);
+            res.revalidate("/customers");
+            res.revalidate(`/customers/${createdId}`);
+            res.status(201).json(createdId);
+        }
+    } catch (error) {
+        console.log("BACK ERR");
+    }
+}
